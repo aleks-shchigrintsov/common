ac37dbe7c159287ceece8833f0cb1d984aa609ab
Aleksandr Shchigrintsov <aleksandr.s@starofservice.com>
2020-07-03 10:21:27 +0200

    Updates

---
+++ src/utils/html-parser.ts
@@ -0,0 +1,23 @@
+import jsdom from 'jsdom';
+
+const { JSDOM } = jsdom;
+
+class HTMLParser {
+  textHtml: string;
+  jsDOM: any;
+
+  constructor(textHtml: string) {
+    this.textHtml = textHtml;
+    this.jsDOM = new JSDOM(textHtml);
+  }
+
+  getDOM() {
+    return new JSDOM(this.textHtml);
+  }
+
+  getTextHtml() {
+    return this.textHtml;
+  }
+}
+
+export default HTMLParser;
